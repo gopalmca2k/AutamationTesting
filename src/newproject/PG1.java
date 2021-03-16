58d44b91e7674361a94c71a9ce38fc9cb2a40ee3
admin <admin@solanki>
2021-03-16 11:31:54 +0530

    "Running Automation Scenarios GUI environment Completed"

--- src/newproject/PG1.java
+++ src/newproject/PG1.java
@@ -1,18 +1,24 @@
 package newproject;
+import javax.swing.JOptionPane;
 import org.openqa.selenium.WebDriver;
 //import org.openqa.selenium.firefox.FirefoxDriver;
 //comment the above line and uncomment below line to use Chrome
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;  
 public class PG1 {
+    String driverFinal;
+    String urlFinal;
 
-
-    public static void main(String[] args)throws InterruptedException {
+    PG1(String d,String url){
+        driverFinal = d;
+        urlFinal = url;
+    }
+    public void runTest()throws InterruptedException {
         // declaration and instantiation of objects/variables
     	//System.setProperty("webdriver.gecko.driver","/home/mu/Gopal/gekodriver/geckodriver");
 	//WebDriver driver = new FirefoxDriver();
 	//comment the above 2 lines and uncomment below 2 lines to use Chrome
-	System.setProperty("webdriver.chrome.driver","/home/mu/Gopal/chromedriver/chromedriver");
+	System.setProperty(driverFinal,urlFinal);
 	WebDriver driver = new ChromeDriver();
     	
         String baseUrl = "http://demo.guru99.com/test/newtours/";
@@ -30,9 +36,9 @@ public class PG1 {
          * the result as "Passed" or "Failed"
          */
         if (actualTitle.contentEquals(expectedTitle)){
-            System.out.println("Test Passed!");
+            JOptionPane.showMessageDialog(null,"Test Passed");
         } else {
-            System.out.println("Test Failed");
+            JOptionPane.showMessageDialog(null,"Test Failed");
         }
        
         //close Fire fox
